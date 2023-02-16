89ce23ca22cdd399652b03e6081fe612e26bc775
Kirsawka <83959481+Kirsawka@users.noreply.github.com>
2023-02-16 21:28:53 +0300

    change user avatar without photo

--- src/components/UserAvatar.tsx
+++ src/components/UserAvatar.tsx
@@ -1,18 +1,17 @@
 import React from 'react';
 import { Avatar } from '@vkontakte/vkui';
 import { useAppSelector } from '../store/hooks';
-import { Icon28Users } from '@vkontakte/icons';
+import { Icon56UserCircleOutline } from '@vkontakte/icons';
 
 function UserAvatar() {
   const user = useAppSelector((state) => state.user.value);
   return (
     <>
-      {user.photoURL.length !== 0 ? (
+      {user.photoURL ? (
         <Avatar size={64} src={user.photoURL} className="user-avatar" />
       ) : (
-        <Avatar size={48} fallbackIcon={<Icon28Users />} src="#" className="user-avatar" />
+        <Icon56UserCircleOutline className="user-avatar" />
       )}
-      <Avatar size={64} src={user.photoURL} className="user-avatar" />
     </>
   );
 }
